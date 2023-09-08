066c19573c7df9a8b49e88e768f7e0b55c796e62
AbrahamMzansie <abraham.nkomo@gmail.com>
2023-09-08 08:48:35 +0200

    unused import

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
 import { SelectedPage } from '../../shared/types';
 import ActionButton from '../../shared/ActionButton';
 import HomePageText from "../../assets/HomePageText.png";
@@ -9,6 +8,8 @@ import SponsorFortune from "../../assets/SponsorFortune.png";
 import SponsorForbes from "../../assets/SponsorForbes.png";
 import useMediaQuery from '../../hooks/useMediaQuery';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
+
 
 
 type Props = {
@@ -24,7 +25,13 @@ const Home = ({setSelectedPage}: Props) => {
      pb-10 pt-10 md:h-full md:pb-0'>
         <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
             <div className='z-10 mt-32 md:basis-3/5'>
-                <div className='md:-mt-20'>
+                <motion.div className='md:-mt-20' initial="hidden" whileInView="visible"
+                viewport={{once:true , amount :0.5}} transition={{duration :0.5}}
+                variants={{hidden : {
+                    opacity :0 , x : -100,
+                   
+                } ,  visible : {opacity :1 , x : 0}}}
+                >
                     <div className='relative'>
                         <div className='before:absolute before:-top-20
                          before:-left-20 before:z-[-1] md:before:content-evolvetext'>
@@ -36,7 +43,7 @@ const Home = ({setSelectedPage}: Props) => {
                     Studios to get the Body Shapes That you Dream of.. Get Your Dream
                     Body Now.
                     </p>
-                </div>
+                </motion.div>
                 {/* {Actions} */}
                 <div className='mt-8 flex items-center gap-8 md:justify-start'>
                     <ActionButton  setSelectedPage={setSelectedPage}>Join Now</ActionButton>
@@ -46,16 +53,16 @@ const Home = ({setSelectedPage}: Props) => {
                    
                 </div>
             </div>
-            <div>
+            <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                 {/* IMAGE */}
             <img src={HomePageGraphic} alt="home-graphics" />
             </div>
         </div>
 
         {isAboveMediumScreens && (
-            <div>
-                <div>
-                    <div>
+            <div className='h-[150px]w-full bg-primary-100 py-10'>
+                <div className='mx-auto w-5/6'>
+                    <div className='flex w-3/5 items-center justify-between gap-8'>
                         <img src={SponsorRedBull} alt="red-bull-sponsoer" />
                         <img src={SponsorFortune} alt="fortune-sponsoer" />
                         <img src={SponsorForbes} alt="forbes-sponsoer" />
